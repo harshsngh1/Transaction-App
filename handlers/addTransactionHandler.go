ceea20252fb6d93006eef39facf7260e9cb50147
Harsh Singh <hs.harsh9919@gmail.com>
2024-08-26 13:31:33 +0530

    added comments for wrong error codes, so that this can be taken care from now onwards

--- handlers/addTransactionHandler.go
+++ handlers/addTransactionHandler.go
@@ -11,7 +11,7 @@ import (
 func AddTransaction(c echo.Context) error {
 	transactionId, err := strconv.ParseInt(c.Param("transaction_id"), 10, 64)
 	if err != nil {
-		return c.JSON(http.StatusBadGateway, "Invalid transaction Id")
+		return c.JSON(http.StatusBadRequest, "Invalid transaction Id") //Use 400 Bad Request (http.StatusBadRequest) because the client provided an invalid input (the transaction ID).
 	}
 	var transaction models.Transaction
 	if err := c.Bind(&transaction); err != nil {
